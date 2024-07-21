2fc73ef005e727287baeb8e1c40762989522d9c7
Praneeth Palugula <praneeth.palugula@gmail.com>
2024-07-21 22:42:35 +0530

    updated the cursor

--- components/Cursor.jsx
+++ components/Cursor.jsx
@@ -22,6 +22,115 @@ const Cursor = () => {
     const requestRef = useRef(null);
 
     useEffect(() => {
+        const mouseOverEvent = () => {
+            cursorEnlarged.current = true;
+            toggleCursorSize();
+        };
+
+        const mouseOutEvent = () => {
+            cursorEnlarged.current = false;
+            toggleCursorSize();
+        };
+
+        const mouseEnterEvent = () => {
+            cursorVisible.current = true;
+            toggleCursorVisibility();
+        };
+
+        const mouseLeaveEvent = () => {
+            cursorVisible.current = false;
+            toggleCursorVisibility();
+        };
+
+        const mouseMoveEvent = e => {
+            cursorVisible.current = true;
+            toggleCursorVisibility();
+
+            endX.current = e.pageX;
+            endY.current = e.pageY;
+
+            dot.current.style.top = endY.current + 'px';
+            dot.current.style.left = endX.current + 'px';
+        };
+
+        const buttonHoverEvent = () => {
+            cursorRectangular.current = true;
+            toggleCursorShape();
+        };
+
+        const buttonLeaveEvent = () => {
+            cursorRectangular.current = false;
+            toggleCursorShape();
+        };
+
+        const headingHoverEvent = () => {
+            cursorScaled.current = true;
+            toggleCursorScale();
+        };
+
+        const headingLeaveEvent = () => {
+            cursorScaled.current = false;
+            toggleCursorScale();
+        };
+
+        const toggleCursorVisibility = () => {
+            if (cursorVisible.current) {
+                dot.current.style.opacity = 1;
+                dotOutline.current.style.opacity = 1;
+            } else {
+                dot.current.style.opacity = 0;
+                dotOutline.current.style.opacity = 0;
+            }
+        };
+
+        const toggleCursorSize = () => {
+            if (cursorEnlarged.current) {
+                dot.current.style.transform = 'translate(-50%, -50%) scale(0.75)';
+                dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1.5)';
+            } else {
+                dot.current.style.transform = 'translate(-50%, -50%) scale(1)';
+                dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1)';
+            }
+        };
+
+        const toggleCursorShape = () => {
+            if (cursorRectangular.current) {
+                dot.current.style.width = '100px'; // Example size, adjust as needed
+                dot.current.style.height = '40px'; // Example size, adjust as needed
+                dot.current.style.borderRadius = '10px'; // Example rounded rectangle, adjust as needed
+                dotOutline.current.style.width = '120px'; // Example size, adjust as needed
+                dotOutline.current.style.height = '60px'; // Example size, adjust as needed
+                dotOutline.current.style.borderRadius = '10px'; // Example rounded rectangle, adjust as needed
+            } else {
+                dot.current.style.width = '30px';
+                dot.current.style.height = '30px';
+                dot.current.style.borderRadius = '50%';
+                dotOutline.current.style.width = '50px';
+                dotOutline.current.style.height = '50px';
+                dotOutline.current.style.borderRadius = '50%';
+            }
+        };
+
+        const toggleCursorScale = () => {
+            if (cursorScaled.current) {
+                dot.current.style.transform = 'translate(-50%, -50%) scale(2)';
+                dotOutline.current.style.opacity = 0;
+            } else {
+                toggleCursorSize();
+                dotOutline.current.style.opacity = 1;
+            }
+        };
+
+        const animateDotOutline = () => {
+            _x.current += (endX.current - _x.current) / delay;
+            _y.current += (endY.current - _y.current) / delay;
+
+            dotOutline.current.style.top = _y.current + 'px';
+            dotOutline.current.style.left = _x.current + 'px';
+
+            requestRef.current = requestAnimationFrame(animateDotOutline);
+        };
+
         document.addEventListener('mousedown', mouseOverEvent);
         document.addEventListener('mouseup', mouseOutEvent);
         document.addEventListener('mousemove', mouseMoveEvent);
@@ -64,115 +173,6 @@ const Cursor = () => {
             cancelAnimationFrame(requestRef.current);
         };
     }, []);
-
-    const toggleCursorVisibility = () => {
-        if (cursorVisible.current) {
-            dot.current.style.opacity = 1;
-            dotOutline.current.style.opacity = 1;
-        } else {
-            dot.current.style.opacity = 0;
-            dotOutline.current.style.opacity = 0;
-        }
-    };
-
-    const toggleCursorSize = () => {
-        if (cursorEnlarged.current) {
-            dot.current.style.transform = 'translate(-50%, -50%) scale(0.75)';
-            dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1.5)';
-        } else {
-            dot.current.style.transform = 'translate(-50%, -50%) scale(1)';
-            dotOutline.current.style.transform = 'translate(-50%, -50%) scale(1)';
-        }
-    };
-
-    const toggleCursorShape = () => {
-        if (cursorRectangular.current) {
-            dot.current.style.width = '100px'; // Example size, adjust as needed
-            dot.current.style.height = '40px'; // Example size, adjust as needed
-            dot.current.style.borderRadius = '10px'; // Example rounded rectangle, adjust as needed
-            dotOutline.current.style.width = '120px'; // Example size, adjust as needed
-            dotOutline.current.style.height = '60px'; // Example size, adjust as needed
-            dotOutline.current.style.borderRadius = '10px'; // Example rounded rectangle, adjust as needed
-        } else {
-            dot.current.style.width = '30px';
-            dot.current.style.height = '30px';
-            dot.current.style.borderRadius = '50%';
-            dotOutline.current.style.width = '50px';
-            dotOutline.current.style.height = '50px';
-            dotOutline.current.style.borderRadius = '50%';
-        }
-    };
-
-    const toggleCursorScale = () => {
-        if (cursorScaled.current) {
-            dot.current.style.transform = 'translate(-50%, -50%) scale(2)';
-            dotOutline.current.style.opacity = 0;
-        } else {
-            toggleCursorSize();
-            dotOutline.current.style.opacity = 1;
-        }
-    };
-
-    const mouseOverEvent = () => {
-        cursorEnlarged.current = true;
-        toggleCursorSize();
-    };
-
-    const mouseOutEvent = () => {
-        cursorEnlarged.current = false;
-        toggleCursorSize();
-    };
-
-    const mouseEnterEvent = () => {
-        cursorVisible.current = true;
-        toggleCursorVisibility();
-    };
-
-    const mouseLeaveEvent = () => {
-        cursorVisible.current = false;
-        toggleCursorVisibility();
-    };
-
-    const mouseMoveEvent = e => {
-        cursorVisible.current = true;
-        toggleCursorVisibility();
-
-        endX.current = e.pageX;
-        endY.current = e.pageY;
-
-        dot.current.style.top = endY.current + 'px';
-        dot.current.style.left = endX.current + 'px';
-    };
-
-    const buttonHoverEvent = () => {
-        cursorRectangular.current = true;
-        toggleCursorShape();
-    };
-
-    const buttonLeaveEvent = () => {
-        cursorRectangular.current = false;
-        toggleCursorShape();
-    };
-
-    const headingHoverEvent = () => {
-        cursorScaled.current = true;
-        toggleCursorScale();
-    };
-
-    const headingLeaveEvent = () => {
-        cursorScaled.current = false;
-        toggleCursorScale();
-    };
-
-    const animateDotOutline = () => {
-        _x.current += (endX.current - _x.current) / delay;
-        _y.current += (endY.current - _y.current) / delay;
-
-        dotOutline.current.style.top = _y.current + 'px';
-        dotOutline.current.style.left = _x.current + 'px';
-
-        requestRef.current = requestAnimationFrame(animateDotOutline);
-    };
 
     return (
         <>
